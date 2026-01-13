dd4c2158e699c31557db900793a3901b6073d1bc
rugalo798 <tiagovinicius79810@gmail.com>
2026-01-13 16:46:53 -0300

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -131,5 +131,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CSRF_TRUSTED_ORIGINS = [
     "https://7e10f1b6eb88.ngrok-free.app",  # substitua pelo seu ngrok
     "https://loja-production-f44c.up.railway.app",
-    "*"
+    "https://3kgames-store.up.railway.app"
 ]
